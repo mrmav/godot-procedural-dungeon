090970bddb3e9910c66514c30a19104d4309a0a7
mrmav <jorgemnoro@outlook.pt>
2021-07-18 08:27:40 +0100

    added cursor image

---
+++ Player/Cursor.cs
@@ -0,0 +1,42 @@
+using Godot;
+using System;
+
+public class Cursor : Sprite
+{
+    
+    [Export]
+    public Texture CursorTexture = null;
+
+    [Export]
+    public Vector2 CursorOffset = Vector2.Zero;
+
+    [Export]
+    public NodePath PlayerPath;
+
+    private Player _player;
+
+    public override void _Ready()
+    {
+        Texture = CursorTexture;
+        ZIndex = 99;
+
+        _player = GetNode<Player>(PlayerPath);
+
+        Input.SetMouseMode(Input.MouseMode.Hidden);
+    }
+
+    public override void _Process(float delta)
+    {
+        
+        if(_player.Control == Player.ControlType.Keyboard)
+        {
+            GlobalPosition = GetGlobalMousePosition() + CursorOffset;
+
+        } else if(_player.Control == Player.ControlType.Controller)
+        {
+            GlobalPosition = _player.CameraFocusPoint;
+        }
+        
+        base._Process(delta);
+    }
+}
